fe6587981847e9687cb6dbfa3628f9da6395156f
Balazs Banto <balazsbanto@gmail.com>
2024-04-19 19:13:42 +0200

    added bypass flag

--- ios/RCTAudioEngineModule.swift
+++ ios/RCTAudioEngineModule.swift
@@ -16,6 +16,7 @@ class RCTAudioEngineModule : NSObject {
   let audioGraph = SBAudioGraph()
   
   lazy var room = Room(delegate: self)
+  var audioProcessor: AudioCustomProcessingDelegate = MyAudioProcessor()
 
   override init() {
     super.init()
@@ -24,7 +25,6 @@ class RCTAudioEngineModule : NSObject {
   
   @objc
   func initSDK() {
-//    start()
     do {
       try AVAudioSession.sharedInstance().setCategory(.playAndRecord)
     } catch {
@@ -35,9 +35,12 @@ class RCTAudioEngineModule : NSObject {
   @objc(connectToRoom:token:)
   func connectToRoom(wsURL: String, token: String) {
     print("Connecting to room with URL \(wsURL) and token \(token)")
+   AudioManager.shared.capturePostProcessingDelegate = audioProcessor
+
     Task {
         do {
             try await room.connect(url: wsURL, token: token)
+//            AudioManager.shared.capturePostProcessingDelegate = audioProcessor
             try await room.localParticipant.setMicrophone(enabled: true)
         } catch {
             print("Failed to connect: \(error)")
@@ -52,19 +55,9 @@ class RCTAudioEngineModule : NSObject {
 
   @objc(enableVoicemod:)
   func enableVoicemod(enable: Bool) {
-      print("Voice modulation enabled: \(enable)")
-  }  
-  
-
-  func start() {
-//    audioEngine.start(audioGraph)
+    print("Voice modulation enabled: \(enable)")
+    (audioProcessor as? MyAudioProcessor)?.bypassCapturePostProcessing = !enable
   }
-
-  func stop() {
-//    audioEngine.stop()
-  }
-
-
 
   @objc
   static func requiresMainQueueSetup() -> Bool {
@@ -84,5 +77,42 @@ extension RCTAudioEngineModule: RoomDelegate {
         guard let track = publication.track as? VideoTrack else {
           return
         }
+    }
+}
+
+class MyAudioProcessor: AudioCustomProcessingDelegate {
+  var initSampleRate: Double = 0.0
+  var initChannels: Int = 0
+  var bypassCapturePostProcessing = true
+  
+  func setBypassForCapturePostProcessing(bypass: Bool) {
+    self.bypassCapturePostProcessing = bypass
+  }
+
+    func audioProcessingInitialize(sampleRate: Int, channels: Int) {
+      initSampleRate = Double(sampleRate)
+      initChannels = channels
+      print("audioProcessingInitialize ")
+
+        // Initialization code here
+    }
+
+    func audioProcessingProcess(audioBuffer: LiveKit.LKAudioBuffer) {
+      if (!self.bypassCapturePostProcessing) {
+        let channelCount = audioBuffer.channels
+        let frameCount = audioBuffer.frames
+
+        for channel in 0..<channelCount {
+            let samples = audioBuffer.rawBuffer(for: channel)
+            for frame in 0..<frameCount {
+                // Halve the volume by multiplying each sample by 0.5
+                samples[frame] *= 0.1
+            }
+        }
+      }
+    }
+
+    func audioProcessingRelease() {
+        // Release resources if needed
     }
 }
